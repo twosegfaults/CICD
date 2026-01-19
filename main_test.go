f11edf16c88f6c200aca4a61da464518761a13b0
Parth Sudan <parth.sudan7@gmail.com>
2026-01-19 11:08:03 +0530

    experimentation3
    fixed faulty test

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestAdd(t *testing.T) {
-	expected := 4
+	expected := 3
 	result := Add(1, 2)
 	if result != expected {
 		t.Errorf("Test Failed: Expected %d, but got %d", expected, result)
